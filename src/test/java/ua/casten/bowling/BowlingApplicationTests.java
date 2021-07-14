652907a6e200d33008dcd11e21a658d803554055
Oleksandr Borovyk <maingroon@gmail.com>
2021-07-14 20:55:17 +0300

    Created maven project and added dependencies

---
+++ src/test/java/ua/casten/bowling/BowlingApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.casten.bowling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
