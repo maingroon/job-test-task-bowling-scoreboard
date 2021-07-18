45a4a8d54856d82f9d56cd904c8eb6f7e0142604
Oleksandr Borovyk <maingroon@gmail.com>
2021-07-18 11:39:23 +0300

    Added ViewFrame class for comfortable display Frame

---
+++ src/main/java/ua/casten/bowling/model/ViewFrame.java
@@ -0,0 +1,22 @@
+package ua.casten.bowling.model;
+
+import lombok.Getter;
+
+@Getter
+public class ViewFrame {
+
+    private final String firstRoll;
+    private final String secondRoll;
+    private final String thirdRoll;
+    private final int score;
+
+    private boolean inGame;
+
+    public ViewFrame(String firstRoll, String secondRoll, String thirdRoll, int score, boolean inGame) {
+        this.firstRoll = firstRoll;
+        this.secondRoll = secondRoll;
+        this.thirdRoll = thirdRoll;
+        this.score = score;
+        this.inGame = inGame;
+    }
+}
