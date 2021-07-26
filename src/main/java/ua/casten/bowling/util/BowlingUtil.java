311775f993abc13d59a490b974c4b9e682276743
Oleksandr Borovyk <maingroon@gmail.com>
2021-07-26 19:38:54 +0300

    Updated BowlingUtil

--- src/main/java/ua/casten/bowling/util/BowlingUtil.java
+++ src/main/java/ua/casten/bowling/util/BowlingUtil.java
@@ -1,9 +1,7 @@
 package ua.casten.bowling.util;
 
-import ua.casten.bowling.model.Frame;
-import ua.casten.bowling.model.Game;
-import ua.casten.bowling.model.ViewFrame;
-
+import ua.casten.bowling.exception.BowlingException;
+import ua.casten.bowling.model.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,44 +18,60 @@ public class BowlingUtil {
     private BowlingUtil() {
     }
 
-    public static List<Frame> sortFrames(List<Frame> frameList) {
+    public static List<RegularFrame> sortFrames(List<RegularFrame> frameList) {
         return frameList.stream()
-                .sorted(comparing(Frame::getNumber))
+                .sorted(comparing(RegularFrame::getNumber))
                 .collect(toList());
     }
 
-    public static List<ViewFrame> parseFrames(Game game) {
-        var frameList = sortFrames(game.getFrames());
-        List<ViewFrame> viewFrames = new ArrayList<>();
+    public static int validateScore(String stringScore) throws BowlingException {
+        int score;
 
-        int i = 0;
-        for (; i < frameList.size(); i++) {
-            viewFrames.add(parseFrame(frameList.get(i)));
+        try {
+            score = Integer.parseInt(stringScore.trim());
+        } catch (NumberFormatException e) {
+            throw new BowlingException("Enter valid score (without symbols and spaces).");
         }
 
-        for (; i < 10; i++) {
-            viewFrames.add(new ViewFrame(EMPTY, EMPTY, EMPTY, -1));
+        if (score < 0 || score > 10) {
+            throw new BowlingException("Score cannot be less than 0 or greater than 10.");
         }
 
-        return viewFrames;
+        return score;
     }
 
-    private static ViewFrame parseFrame(Frame frame) {
-        String firstRoll = parseFirstRoll(frame);
-        String secondRoll = parseSecondRoll(frame);
-        String thirdRoll = parseThirdRoll(frame);
+    public static List<RegularViewFrame> parseRegularFrames(Game game) {
+        var regularFrames = sortFrames(game.getRegularFrames());
+        List<RegularViewFrame> regularViewFrames = new ArrayList<>(9);
+        int i = 0;
 
-        return new ViewFrame(
+        for (; i < regularFrames.size(); i++) {
+            regularViewFrames.add(parseRegularFrame(regularFrames.get(i)));
+        }
+
+        for (; i < 9; i++) {
+            regularViewFrames.add(new RegularViewFrame(EMPTY, EMPTY, -1));
+        }
+
+        return regularViewFrames;
+    }
+
+    private static RegularViewFrame parseRegularFrame(RegularFrame regularFrame) {
+        var firstRoll = parseRegularFirstFrame(regularFrame);
+        var secondRoll = parseRegularSecondFrame(regularFrame);
+        var score = regularFrame.getScore();
+
+        return new RegularViewFrame(
                 firstRoll,
                 secondRoll,
-                thirdRoll,
-                frame.getScore() == null ? 0 : frame.getScore()
+                score
         );
     }
 
-    private static String parseFirstRoll(Frame frame) {
-        var firstRoll = frame.getFirstRoll();
-        if (firstRoll == null || (frame.isStrike() && frame.getNumber() != 10)) {
+    private static String parseRegularFirstFrame(RegularFrame regularFrame) {
+        var firstRoll = regularFrame.getFirstRoll();
+
+        if (firstRoll == null || regularFrame.isStrike()) {
             return EMPTY;
         }
 
@@ -65,16 +79,13 @@ public class BowlingUtil {
             return MISS;
         }
 
-        if (frame.isStrike() && frame.getNumber() == 10) {
-            return STRIKE;
-        }
-
         return firstRoll.toString();
     }
 
-    private static String parseSecondRoll(Frame frame) {
-        var secondRoll = frame.getSecondRoll();
-        if (frame.getFirstRoll() != null && secondRoll == null && frame.isStrike() && frame.getNumber() != 10) {
+    private static String parseRegularSecondFrame(RegularFrame regularFrame) {
+        var secondRoll = regularFrame.getSecondRoll();
+
+        if (regularFrame.getFirstRoll() != null && regularFrame.isStrike()) {
             return STRIKE;
         }
 
@@ -86,20 +97,80 @@ public class BowlingUtil {
             return MISS;
         }
 
-        if (secondRoll == 10 && frame.getFirstRoll() != null && frame.getFirstRoll() == 10) {
-            return STRIKE;
-        }
-
-        if (frame.isSpare()) {
+        if (regularFrame.isSpare()) {
             return SPARE;
         }
 
         return secondRoll.toString();
     }
 
-    private static String parseThirdRoll(Frame frame) {
-        var thirdRoll = frame.getThirdRoll();
-        if (frame.getSecondRoll() == null || thirdRoll == null) {
+    public static LastViewFrame parseLastFrame(Game game) {
+        var lastFrame = game.getLastFrame();
+
+        var firstRoll = EMPTY;
+        var secondRoll = EMPTY;
+        var thirdRoll = EMPTY;
+        var score = -1;
+
+        if (lastFrame != null) {
+            firstRoll = parseLastFrameFirstRoll(lastFrame);
+            secondRoll = parseLastFrameSecondRoll(lastFrame);
+            thirdRoll = parseLastFrameFThirdRoll(lastFrame);
+            score = lastFrame.getScore();
+        }
+
+        return new LastViewFrame(
+                firstRoll,
+                secondRoll,
+                thirdRoll,
+                score
+        );
+    }
+
+    private static String parseLastFrameFirstRoll(LastFrame lastFrame) {
+        var firstRoll = lastFrame.getFirstRoll();
+
+        if (firstRoll == null) {
+            return EMPTY;
+        }
+
+        if (firstRoll == 10) {
+            return STRIKE;
+        }
+
+        if (firstRoll == 0) {
+            return MISS;
+        }
+
+        return firstRoll.toString();
+    }
+
+    private static String parseLastFrameSecondRoll(LastFrame lastFrame) {
+        var secondRoll = lastFrame.getSecondRoll();
+
+        if (secondRoll == null) {
+            return EMPTY;
+        }
+
+        if (secondRoll == 10 && lastFrame.getFirstRoll() != 0) {
+            return STRIKE;
+        }
+
+        if (secondRoll == 0) {
+            return MISS;
+        }
+
+        if (secondRoll + lastFrame.getFirstRoll() == 10) {
+            return SPARE;
+        }
+
+        return secondRoll.toString();
+    }
+
+    private static String parseLastFrameFThirdRoll(LastFrame lastFrame) {
+        var thirdRoll = lastFrame.getThirdRoll();
+
+        if (thirdRoll == null) {
             return EMPTY;
         }
 
@@ -107,15 +178,91 @@ public class BowlingUtil {
             return MISS;
         }
 
-        if (thirdRoll == 10 && frame.getSecondRoll() != 0) {
+        if (thirdRoll == 10 && lastFrame.getSecondRoll() != 0) {
             return STRIKE;
         }
 
-        if (thirdRoll + frame.getSecondRoll() == 10) {
+        if (thirdRoll + lastFrame.getSecondRoll() == 10) {
             return SPARE;
         }
 
         return thirdRoll.toString();
     }
 
+//    private static ViewFrame parseFrame(Frame frame) {
+//        String firstRoll = parseFirstRoll(frame);
+//        String secondRoll = parseSecondRoll(frame);
+//        String thirdRoll = parseThirdRoll(frame);
+//
+//        return new ViewFrame(
+//                firstRoll,
+//                secondRoll,
+//                thirdRoll,
+//                frame.getScore() == null ? 0 : frame.getScore()
+//        );
+//    }
+//
+//    private static String parseFirstRoll(Frame frame) {
+//        var firstRoll = frame.getFirstRoll();
+//        if (firstRoll == null || (frame.isStrike() && frame.getNumber() != 10)) {
+//            return EMPTY;
+//        }
+//
+//        if (firstRoll == 0) {
+//            return MISS;
+//        }
+//
+//        if (frame.isStrike() && frame.getNumber() == 10) {
+//            return STRIKE;
+//        }
+//
+//        return firstRoll.toString();
+//    }
+//
+//    private static String parseSecondRoll(Frame frame) {
+//        var secondRoll = frame.getSecondRoll();
+//        if (frame.getFirstRoll() != null && secondRoll == null && frame.isStrike() && frame.getNumber() != 10) {
+//            return STRIKE;
+//        }
+//
+//        if (secondRoll == null) {
+//            return EMPTY;
+//        }
+//
+//        if (secondRoll == 0) {
+//            return MISS;
+//        }
+//
+//        if (secondRoll == 10 && frame.getFirstRoll() != null && frame.getFirstRoll() == 10) {
+//            return STRIKE;
+//        }
+//
+//        if (frame.isSpare()) {
+//            return SPARE;
+//        }
+//
+//        return secondRoll.toString();
+//    }
+//
+//    private static String parseThirdRoll(Frame frame) {
+//        var thirdRoll = frame.getThirdRoll();
+//        if (frame.getSecondRoll() == null || thirdRoll == null) {
+//            return EMPTY;
+//        }
+//
+//        if (thirdRoll == 0) {
+//            return MISS;
+//        }
+//
+//        if (thirdRoll == 10 && frame.getSecondRoll() != 0) {
+//            return STRIKE;
+//        }
+//
+//        if (thirdRoll + frame.getSecondRoll() == 10) {
+//            return SPARE;
+//        }
+//
+//        return thirdRoll.toString();
+//    }
+//
 }
